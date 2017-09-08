d3d0b7504ef5dca40541e9ff2b78c977280e2616
Sergey Naumenko <sernmk@gmail.com>
2017-09-08 12:16:31 +0300

    Create wordsCount.php

---
+++ wordsCount.php
@@ -0,0 +1,24 @@
+/*
+* This function takes a string and counts how many times each word is repeated.
+* Example use:
+*    print_r(wordsCount('one tree two tree   two  tree two  bag   '));
+*    result:
+*            Array
+* (
+*                [one] => 1
+*                [tree] => 3
+*                [two] => 3
+*                [bag] => 1
+* )
+*/
+function wordsCount($string)
+{
+    $result = explode(" ", $string);
+    $newArray = [];
+    foreach ($result as $value) {
+        if ($value != '') {
+            !array_key_exists($value, $newArray) ? $newArray[$value] = 1 : $newArray[$value] += 1;            
+        }
+    }
+    return $newArray;
+}
