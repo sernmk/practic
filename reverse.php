e56260df599c3fc646bf898f3effd0b86d68a478
Sergey Naumenko <sernmk@gmail.com>
2017-09-08 06:23:00 +0300

    Create reverse.php

---
+++ reverse.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* This function, which receives a string and returns it inverted.
+*/
+function reverse($string) {
+    $result = '';
+    for ($i = strlen($string) - 1; $i >= 0; $i--) {
+        $result = $result.$string[$i];
+    }
+    return $result;
+}
